acc61396bdf5c1f9308f92f3fef9c4c58badc753
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-10-27 22:11:07 +0530

    Add question prefetch functionality

--- src/xapi/questionManager.js
+++ src/xapi/questionManager.js
@@ -10,12 +10,26 @@ async function getNextQuestion() {
   if (questionsString && questionsString.length > 0) {
     questions = JSON.parse(questionsString);
   }
+  // This will happen only at the beginning of the session,
+  // or if somehow no questions could be fetched until all are gone
   if (!questions || questions.length <= 0) {
     questions = await ApiManager.apiManager.getUserUnansweredQuestions();
   }
 
   const question = questions.shift();
   sessionStorage.setItem('questions', JSON.stringify(questions));
+  // Preload if one or nothing left
+  // This happens in background and reduces loading time
+  if (questions.length <= 1) {
+    ApiManager.apiManager
+      .getUserUnansweredQuestions()
+      .then(preloadedQuestions => {
+        sessionStorage.setItem(
+          'questions',
+          JSON.stringify(questions.concat(preloadedQuestions))
+        );
+      });
+  }
 
   return question;
 }
